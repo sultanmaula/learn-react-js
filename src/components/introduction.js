a711d5b1c2bd8b1de3f8534a30ffc7b3d387ae55
Sultan Maula <sultanmaulachamzah@gmail.com>
2023-05-06 22:10:38 +0700

    learn session 1

---
+++ src/components/introduction.js
@@ -0,0 +1,10 @@
+function Introduction(props) {
+    return (
+    <div>
+        <h1>INTRODUCTION</h1>
+        <p>Introduction description, my name is : {props.name}</p>
+    </div>
+    );
+}
+
+export default Introduction;
